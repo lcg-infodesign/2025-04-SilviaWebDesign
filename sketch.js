2106da49a71bca3972f70673c0539ee0d8f99b65
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-11-04 15:39:49 +0100

    file cambati

--- sketch.js
+++ sketch.js
@@ -1,17 +1,58 @@
+let data;
+let latitudineMinima, longitudineMinima, latitudineMassima, longitudineMassima;
+let margine = 20;
+
 function preload() {
-  // put preload code here
+  data = loadTable("assets/sipri-report-explosions.csv", "CSV", "header");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  
+  // deifnisco valori minimi e massimi per la latitudine
+  let tutteLeLatitudini = data.getColumn("latitude");
+  latitudineMinima = min(tutteLeLatitudini);
+  latitudineMassima = max(tutteLeLatitudini);
+
+  //deifnisco valori minimi e massimi per la lagitudine
+  let tutteLeLongitudini = data.getColumn("longitude");
+  longitudineMinima = min(tutteLeLongitudini);
+  longitudineMassima = max(tutteLeLongitudini);
+
+  console.log(longitudineMassima);
 }
 
 function draw() {
-  // put drawing code here
+  background(10);
+
+  for (let i=0; i<data.getRowCount(); i++) {
+    //leggo i dati della singola riga
+    let longitudine = data.getNum(i, "longitude");
+    let latitudine = data.getNum(i, "latitude");
+    let paese = data.getString(i, "country");
+    
+    // converto le coordinate geografiche in pixel
+    let x = map(longitudine, longitudineMinima, longitudineMassima, margine, width-margine);
+    let y = map(latitudine, latitudineMinima, latitudineMassima, height-margine, margine);
+    let raggio = 10;
+
+    // calcolo la distanza
+    let distanza = dist(x, y, mouseX, mouseY);
+
+    if(distanza < raggio) {
+      fill("red");
+    } else {
+      fill("yellow");
+    }
+
+    ellipse(x, y, raggio*2);
+
+    if (distanza < raggio){
+      fill("white");
+      text(paese, x, y);
+    }
+  }
+
+
+
 }
